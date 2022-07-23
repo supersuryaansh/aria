9ac342341e7c1cd90e56dc03d8373e4ceb4d827f
supersuryaansh <prod.genx@gmail.com>
2022-07-23 23:14:08 +0530

    Added the Anime search functionality partially

--- modules/anime.php
+++ modules/anime.php
@@ -1,2 +1,16 @@
 <?php
 //This is the main Anime module
+//Search the Anime
+$discord->registerCommand(
+    "search",
+    function ($message, $params) {
+        echo $params[0];
+        //bash command to search for Anime
+        $command =
+            '/bin/bash -c "'.dirname(__FILE__) .'/../ani-cli -x ' . $params[0] . ' <<< $\'q\\n\'"';
+        exec($command, $output);
+    },
+    [
+        "description" => "Search for an Anime",
+    ]
+);
